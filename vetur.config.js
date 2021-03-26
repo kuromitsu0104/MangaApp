37e2a27dc016250fe5c4a266276596fcb2609154
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-03-26 19:00:55 +0900

    [Fix] Veturの設定ファイルを修正

--- vetur.config.js
+++ vetur.config.js
@@ -16,16 +16,16 @@ module.exports = {
       // **required**
       // Where is your project?
       // It is relative to `vetur.config.js`.
-      root: './frontend/packages/repo1',
+      root: './frontend',
       // **optional** default: `'package.json'`
       // Where is `package.json` in the project?
       // We use it to determine the version of vue.
       // It is relative to root property.
-      package: './frontend/package.json',
+      package: './package.json',
       // **optional**
       // Where is TypeScript config file in the project?
       // It is relative to root property.
-      tsconfig: './frontend/tsconfig.json',
+      tsconfig: './tsconfig.json',
       // **optional** default: `'./.vscode/vetur/snippets'`
       // Where is vetur custom snippets folders?
       snippetFolder: './.vscode/vetur/snippets',
@@ -35,7 +35,7 @@ module.exports = {
       // It is relative to root property.
       // Notice: It won't actually do it. You need to use `require.context` or `Vue.component`
       globalComponents: [
-        './frontend/src/components/**/*.vue'
+        './src/components/**/*.vue'
       ]
     }
   ]
