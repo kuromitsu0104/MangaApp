e3942a60c307888fb82fdf5d9b37b1e6509bc7a1
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-03-26 11:32:49 +0900

    [Add] Todoコンポーネントを追加, routerを追加

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import Todo from "../views/Todo.vue";
+import About from "../views/About.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,8 +15,14 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
+  },
+  {
+    path: "/todo",
+    name: "Todo",
+    component: Todo,
   },
 ];
 
---
+++ frontend/src/views/Todo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="todo">
+    <h1>TODO APP</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Todo",
+});
+</script>
