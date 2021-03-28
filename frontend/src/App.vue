8a5b67c1ea70b312357337867cc0bd095338436a
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-03-28 22:10:23 +0900

    [Add] Headerコンポーネントを作成

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,18 @@
 <template>
+  <Header />
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/components/organisms/Header.vue";
+
+export default defineComponent({
+  components: {
+    Header,
+  },
+});
+</script>
---
+++ frontend/src/components/organisms/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="w-full h-screen">
+    <header class="bg-teal-400">
+      <nav class="flex justify-between w-full bg-green-500 text-white p-4">
+        <a href="/"
+          ><span class="font-semibold text-xl tracking-tight">MangaApp</span></a
+        >
+        <div class="md:items-center md:w-auto flex">
+          <div class="md:flex hidden">
+            <a class="block md:text-white mr-4" href="/link">Link 1</a>
+            <a class="block md:text-white mr-4" href="/link">Link 2</a>
+            <a class="block md:text-white mr-4" href="/link">Link 3</a>
+            <a class="block md:text-white mr-4" href="/link">Link 4</a>
+          </div>
+          <div class="flex text-sm">
+            <a
+              class="p-2 ml-2 bg-white text-teal-500 font-semibold leading-none border border-gray-100 rounded hover:border-transparent hover:bg-gray-100"
+              href="/auth/signin"
+              >Login</a
+            >
+            <a
+              class="p-2 ml-2 bg-teal-500 text-gray-100 font-semibold leading-none border border-teal-600 rounded hover:border-transparent hover:bg-teal-600"
+              href="/auth/signup"
+              >Sign up</a
+            >
+          </div>
+        </div>
+      </nav>
+    </header>
+    <div class="bottomNav fixed bottom-0 w-full">
+      <nav
+        style="border: 1px solid blue"
+        class="md:hidden bottom-0 w-full bg-gray-700 text-xs"
+      >
+        <ul
+          class="flex justify-around items-center text-white text-center opacity-75 text-lg font-bold"
+        >
+          <li class="p-4 hover:bg-gray-500">
+            <a href="/link">
+              <span>Link 1</span>
+              <svg class="h-6 w-6 fill-current mx-auto" viewBox="0 0 20 20">
+                <path
+                  d="M14 8a4 4 0 1 0-8 0v7h8V8zM8.027 2.332A6.003 6.003 0 0 0 4 8v6l-3 2v1h18v-1l-3-2V8a6.003 6.003 0 0 0-4.027-5.668 2 2 0 1 0-3.945 0zM12 18a2 2 0 1 1-4 0h4z"
+                  fill-rule="evenodd"
+                />
+              </svg>
+            </a>
+          </li>
+          <li class="p-4 hover:bg-gray-500">
+            <a href="/link">
+              <span>Link 2</span>
+              <svg class="h-6 w-6 fill-current mx-auto" viewBox="0 0 20 20">
+                <path
+                  d="M14 8a4 4 0 1 0-8 0v7h8V8zM8.027 2.332A6.003 6.003 0 0 0 4 8v6l-3 2v1h18v-1l-3-2V8a6.003 6.003 0 0 0-4.027-5.668 2 2 0 1 0-3.945 0zM12 18a2 2 0 1 1-4 0h4z"
+                  fill-rule="evenodd"
+                />
+              </svg>
+            </a>
+          </li>
+          <li class="p-4 hover:bg-gray-500">
+            <a href="/link">
+              <span>Link 3</span>
+              <svg class="h-6 w-6 fill-current mx-auto" viewBox="0 0 20 20">
+                <path
+                  d="M14 8a4 4 0 1 0-8 0v7h8V8zM8.027 2.332A6.003 6.003 0 0 0 4 8v6l-3 2v1h18v-1l-3-2V8a6.003 6.003 0 0 0-4.027-5.668 2 2 0 1 0-3.945 0zM12 18a2 2 0 1 1-4 0h4z"
+                  fill-rule="evenodd"
+                />
+              </svg>
+            </a>
+          </li>
+          <li class="p-4 hover:bg-gray-500">
+            <a href="/link">
+              <span>Link 4</span>
+              <svg class="h-6 w-6 fill-current mx-auto" viewBox="0 0 20 20">
+                <path
+                  d="M14 8a4 4 0 1 0-8 0v7h8V8zM8.027 2.332A6.003 6.003 0 0 0 4 8v6l-3 2v1h18v-1l-3-2V8a6.003 6.003 0 0 0-4.027-5.668 2 2 0 1 0-3.945 0zM12 18a2 2 0 1 1-4 0h4z"
+                  fill-rule="evenodd"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
